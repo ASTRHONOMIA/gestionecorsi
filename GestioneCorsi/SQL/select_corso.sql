10500f6246af9409f8563d7483aa7fbe2bb49451
andre_yta8ub6 <andre_yta8ub6@Andrea>
2022-11-14 15:38:56 +0100

    DAOCostants modified

--- GestioneCorsi/SQL/select_corso.sql
+++ GestioneCorsi/SQL/select_corso.sql
@@ -4,14 +4,17 @@ select * from corso;
 -- select_corso_by_cod
 select * from corso where cod_corso = ?;
 
--- select_nome_corso_freguente
+-- select_nome_corso_frequente
 select count(nome_corso) as Numero_Corsi from corso;
 
 -- update_corso_by_cod
-update nome_corso = ?, data_inizio = ?, data_fine = ?, costo = ?, cod_docente = ? from corso where cod_corso = ?;
+update corso set nome_corso = ?, data_inizio = ?, data_fine = ?, costo = ?, cod_docente = ? where cod_corso = ?;
 
 -- select_data_inizio_corso
 select data_inizio from corso where cod_corso = ?;
+
+-- select_data_fine_corso
+select data_fine from corso where cod_corso = ?;
 
 -- select_numero_commenti_corso
 select count(commento_corso) as Numero_commenti from corso;
--- GestioneCorsi/src/main/java/com/roma/gestionecorsi/architecture/dao/DAOCostants.java
+++ GestioneCorsi/src/main/java/com/roma/gestionecorsi/architecture/dao/DAOCostants.java
@@ -8,10 +8,16 @@ public interface DAOCostants {
 	String SELECT_CORSO="Select * from corso";
 	String DELETE_CORSO ="Delete from corso where cod_corso = ?";
 	String SELECT_CORSO_BYCOD ="Select * from corso where cod_corso = ?";
+	String SELECT_DATA_INIZIO="select data_inizio from corso where cod_corso = ?";
+	String SELECT_DATA_FINE="select data_fine from corso where cod_corso = ?";
+	String SELECT_NUMERO_COMMENTI="select count(commento_corso) as Numero_commenti from corso";
+	String UPDATE_CORSO_BYCOD="update corso set nome_corso = ?, data_inizio = ?, data_fine = ?, costo = ?, cod_docente = ? where cod_corso = ?";
 
-	String SELECT_CORSISTA="Select * from corsista";
-	String SELECT_CORSISTA_BYCOD ="Select * from corsista where codice_corsista = ?";
-
+	String SELECT_CORSISTA="Select * from corsisti";
+	String SELECT_CORSISTA_BYCOD ="Select * from corsisti where codice_corsista = ?";
+	String SELECT_NUMERO_CORSISTI="Select count(*) from corsisti";
+	String UPDATE_CORSISTI="update corsisti set nome_corsista = ?, cognome_corsista= ?, precedenti_formativi=? where cod_corsista=?";
+	
 	String SELECT_DOCENTE="Select * from docente";
 	String SELECT_DOCENTE_BYCOD ="Select * from docente where cod_docente = ?";
 }

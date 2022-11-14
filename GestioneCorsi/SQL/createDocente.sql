df12fee54f7c274396ab352eb8a520fd79b54721
Betacom <Betacom@Betacom-PCDMTT.homenet.telecomitalia.it>
2022-11-14 12:09:32 +0100

    create/drop table docente

---
+++ GestioneCorsi/SQL/createDocente.sql
@@ -0,0 +1,6 @@
+create table docente(
+nome_docente varchar2(30) not null,
+coognome_docente varchar2(30) not null,
+cv_docente varchar2(50),
+cod_docente long not null unique,
+constraint p_cod_docente primary key(cod_docente));
---
+++ GestioneCorsi/SQL/dropDocente.sql
@@ -0,0 +1,1 @@
+drop table docente cascade constraints;

dfcbfc65c54d7f718119537988221437a8827924
Betacom <Betacom@Betacom-PCDMTT.homenet.telecomitalia.it>
2022-11-14 12:34:25 +0100

    Correzione create_amministratore , create_docente

--- GestioneCorsi/SQL/createDocente.sql
+++ GestioneCorsi/SQL/createDocente.sql
@@ -2,5 +2,5 @@ create table docente(
 nome_docente varchar2(30) not null,
 coognome_docente varchar2(30) not null,
 cv_docente varchar2(50),
-cod_docente long not null unique,
+cod_docente int,
 constraint p_cod_docente primary key(cod_docente));
--- GestioneCorsi/SQL/create_amministratore.sql
+++ GestioneCorsi/SQL/create_amministratore.sql
@@ -1,5 +1,5 @@
 create table amministratore(
 cod_admin int,
-nome_admin varchar2(30);
-cognome_admin varchar2(30);
+nome_admin varchar2(30),
+cognome_admin varchar2(30),
 constraint p_cod_admin primary key(cod_admin));

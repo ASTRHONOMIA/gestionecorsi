c938ce466f2df247bd04891a2777a7235089beef
Emiliano Parrello <e.parrello.esterno@almaviva.it>
2022-11-14 16:54:29 +0100

    correzzione dao

--- GestioneCorsi/src/main/java/com/roma/gestionecorsi/architecture/dao/DAOCostants.java
+++ GestioneCorsi/src/main/java/com/roma/gestionecorsi/architecture/dao/DAOCostants.java
@@ -11,8 +11,7 @@ public interface DAOCostants {
 	String SELECT_DATA_INIZIO="select data_inizio from corso where cod_corso = ?";
 	String SELECT_DATA_FINE="select data_fine from corso where cod_corso = ?";
 	String SELECT_NUMERO_COMMENTI="select count(commento_corso) as Numero_commenti from corso";
-	String UPDATE_CORSO_BYCOD="update corso set nome_corso = ?, data_inizio = ?, data_fine = ?, costo = ?, cod_docente = ? where cod_corso = ?";
-
+	String UPDATE_CORSO_BYCOD="update corso set nome_corso = ?, data_inizio = ?, data_fine = ?, costo_corso = ?, commento_corso = ?, aula_corso = ?, cod_docente = ? where cod_corso = ?";
 	String SELECT_CORSISTA="Select * from corsisti";
 	String SELECT_CORSISTA_BYCOD ="Select * from corsisti where codice_corsista = ?";
 	String SELECT_NUMERO_CORSISTI="Select count(*) from corsisti";

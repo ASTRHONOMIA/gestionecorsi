b80e60c38638675fc8c3aa85cb1ed66d17eb0eb7
Betacom <Betacom@Betacom-PCMXGA.wind3.hub>
2022-11-14 15:14:43 +0100

    dao costants

--- GestioneCorsi/src/main/java/com/roma/gestionecorsi/architecture/dao/DAOCostants.java
+++ GestioneCorsi/src/main/java/com/roma/gestionecorsi/architecture/dao/DAOCostants.java
@@ -1,5 +1,17 @@
 package com.roma.gestionecorsi.architecture.dao;
 
 public interface DAOCostants {
+	
+	String SELECT_ADMIN ="Select * from admin";
+	String SELECT_ADMIN_BYCOD ="Select * from admin where cod_admin = ?";
 
+	String SELECT_CORSO="Select * from corso";
+	String DELETE_CORSO ="Delete from corso where cod_corso = ?";
+	String SELECT_CORSO_BYCOD ="Select * from corso where cod_corso = ?";
+
+	String SELECT_CORSISTA="Select * from corsista";
+	String SELECT_CORSISTA_BYCOD ="Select * from corsista where codice_corsista = ?";
+
+	String SELECT_DOCENTE="Select * from docente";
+	String SELECT_DOCENTE_BYCOD ="Select * from docente where cod_docente = ?";
 }

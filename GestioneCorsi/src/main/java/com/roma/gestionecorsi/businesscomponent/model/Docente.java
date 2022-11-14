8a68b646e8fd9b1ade74eb4509023292d1133aad
Betacom <Betacom@Betacom-PCDMTT.homenet.telecomitalia.it>
2022-11-14 12:43:24 +0100

    model Docente

---
+++ GestioneCorsi/src/main/java/com/roma/gestionecorsi/businesscomponent/model/Docente.java
@@ -0,0 +1,49 @@
+package com.roma.gestionecorsi.businesscomponent.model;
+
+public class Docente {
+	String nomeDocente;
+	String cognomeDocente;
+	String cvDocente;
+	long codDocente;
+	
+	public String getNomeDocente() {
+		return nomeDocente;
+	}
+	
+	public void setNomeDocente(String nomeDocente) {
+		this.nomeDocente = nomeDocente;
+	}
+	
+	public String getCognomeDocente() {
+		return cognomeDocente;
+	}
+	
+	public void setCognomeDocente(String cognomeDocente) {
+		this.cognomeDocente = cognomeDocente;
+	}
+	
+	public String getCvDocente() {
+		return cvDocente;
+	}
+	
+	public void setCvDocente(String cvDocente) {
+		this.cvDocente = cvDocente;
+	}
+	
+	public long getCodDocente() {
+		return codDocente;
+	}
+	
+	public void setCodDocente(long codDocente) {
+		this.codDocente = codDocente;
+	}
+
+	@Override
+	public String toString() {
+		return "Docente [nomeDocente=" + nomeDocente + ", cognomeDocente=" + cognomeDocente + ", cvDocente=" + cvDocente
+				+ ", codDocente=" + codDocente + "]";
+	}
+	
+	
+	
+}

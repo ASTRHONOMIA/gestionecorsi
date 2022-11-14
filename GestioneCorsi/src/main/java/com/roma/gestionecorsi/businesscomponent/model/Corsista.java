1bd1c52f955cbea5974bbd10ac08a7090426a754
andre_yta8ub6 <andre_yta8ub6@Andrea>
2022-11-14 12:38:31 +0100

    Corsista model and server

--- GestioneCorsi/src/main/java/com/roma/gestionecorsi/businesscomponent/model/Corsista.java
+++ GestioneCorsi/src/main/java/com/roma/gestionecorsi/businesscomponent/model/Corsista.java
@@ -1,5 +1,51 @@
 package com.roma.gestionecorsi.businesscomponent.model;
 
 public class Corsista {
+	
+	String nomeCorsista;
+	String cognomeCorsista;
+	long codiceCorsista;
+	private boolean precedentiFormativi;
+	
+	public Corsista() {}
 
+	public String getNomeCorsista() {
+		return nomeCorsista;
+	}
+
+	public void setNomeCorsista(String nomeCorsista) {
+		this.nomeCorsista = nomeCorsista;
+	}
+
+	public String getCognomeCorsista() {
+		return cognomeCorsista;
+	}
+
+	public void setCognomeCorsista(String cognomeCorsista) {
+		this.cognomeCorsista = cognomeCorsista;
+	}
+
+	public long getCodiceCorsista() {
+		return codiceCorsista;
+	}
+
+	public void setCodiceCorsista(long codiceCorsista) {
+		this.codiceCorsista = codiceCorsista;
+	}
+
+	public boolean isPrecedentiFormativi() {
+		return precedentiFormativi;
+	}
+
+	public void setPrecedentiFormativi(boolean precedentiFormativi) {
+		this.precedentiFormativi = precedentiFormativi;
+	}
+
+	@Override
+	public String toString() {
+		return "Corsista [nomeCorsista=" + nomeCorsista + ", cognomeCorsista=" + cognomeCorsista + ", codiceCorsista="
+				+ codiceCorsista + ", precedentiFormativi=" + precedentiFormativi + "]";
+	}
+	
+	
 }

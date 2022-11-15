7e6ab419fbc95f6a7ed4cbfabec6bf0b166a172e
Betacom <Betacom@Betacom-PCDMTT.homenet.telecomitalia.it>
2022-11-15 13:04:19 +0100

    CorsoCorsistaBC and Test

---
+++ GestioneCorsi/src/main/java/com/roma/gestionecorsi/businesscomponent/CorsoCorsistaBC.java
@@ -0,0 +1,22 @@
+package com.roma.gestionecorsi.businesscomponent;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import com.roma.gestionecorsi.architecture.dao.CorsoCorsistaDAO;
+import com.roma.gestionecorsi.architecture.dao.DAOException;
+import com.roma.gestionecorsi.architecture.dbaccess.DBAccess;
+import com.roma.gestionecorsi.businesscomponent.model.CorsoCorsista;
+
+public class CorsoCorsistaBC {
+	private  Connection conn;
+	
+	public CorsoCorsistaBC() throws ClassNotFoundException, DAOException, IOException {
+		conn = DBAccess.getConnection();
+	}
+	
+	public void create(CorsoCorsista cc) throws DAOException{
+		CorsoCorsistaDAO.getFactory().create(conn, cc);
+	}
+
+}
--- GestioneCorsi/src/main/java/test/com/roma/gestionecorsi/businesscomponent/CorsoCorsistaBCTest.java
+++ GestioneCorsi/src/main/java/test/com/roma/gestionecorsi/businesscomponent/CorsoCorsistaBCTest.java
@@ -1,7 +1,8 @@
-package test.com.roma.gestionecorsi.architecture.dao;
+package test.com.roma.gestionecorsi.businesscomponent;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.util.GregorianCalendar;
@@ -10,33 +11,36 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.roma.gestionecorsi.architecture.dao.CorsistiDAO;
-import com.roma.gestionecorsi.architecture.dao.CorsoCorsistaDAO;
-import com.roma.gestionecorsi.architecture.dao.CorsoDAO;
 import com.roma.gestionecorsi.architecture.dao.DAOException;
 import com.roma.gestionecorsi.architecture.dbaccess.DBAccess;
+import com.roma.gestionecorsi.businesscomponent.CorsistaBC;
+import com.roma.gestionecorsi.businesscomponent.CorsoBC;
+import com.roma.gestionecorsi.businesscomponent.CorsoCorsistaBC;
 import com.roma.gestionecorsi.businesscomponent.model.Corsista;
 import com.roma.gestionecorsi.businesscomponent.model.Corso;
 import com.roma.gestionecorsi.businesscomponent.model.CorsoCorsista;
 
-class CorsoCorsistaDAOTest {
+class CorsoCorsistaBCTest {
 	private static Connection conn;
 	private static Corso corso;
 	private static Corsista corsista;
 	private static CorsoCorsista cc;
+	private static CorsoBC cBC;
+	private static CorsistaBC corBC;
+	private static CorsoCorsistaBC ccBC;
 	
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		//Insert Docente
 		conn = DBAccess.getConnection();
-		
-		
-
 		Statement stmt = conn.createStatement();
-		stmt.executeUpdate("Insert into docente values('francesco','Francese','',1)");
+		stmt.executeUpdate("Insert into docente values('Andrea','Tulino','',1)");
 		conn.commit();
 
+		
 		corso = new Corso();
+		cBC = new CorsoBC();
 		
 		corso.setCodCorso(1);
 		corso.setNomeCorso("Inglese");
@@ -47,51 +51,61 @@ class CorsoCorsistaDAOTest {
 		corso.setAulaCorso("N5");
 		corso.setCodDocente(1);
 		
-		corsista =new Corsista();
-		corsista.setNomeCorsista("Franco");
-		corsista.setCognomeCorsista("Molis");
-		corsista.setCodiceCorsista(1);
+		
+		corBC = new CorsistaBC();
+		corsista = new Corsista();
+		corsista.setCognomeCorsista("Tulino");
+		corsista.setNomeCorsista("Andrea");
+		corsista.setPrecedentiFormativi(false);
+		
+		
+		
 		corsista.setPrecedentiFormativi(false);
 		
 		
 		cc = new CorsoCorsista();
-		cc.setCodCorso(1);
-		cc.setCodCorsista(1);
-		
+		ccBC = new CorsoCorsistaBC();
 		
 		
 	}
 	
-
 	@Test
-	void corsoCorsistaTest() throws DAOException {
+	void create() {
 		try {
-			CorsoDAO.getFactory().create(conn, corso);
-			CorsistiDAO.getFactory().create(conn, corsista);
-			CorsoCorsistaDAO.getFactory().create(conn, cc);
-			//System.out.println(cc.toString());
-		}catch (DAOException e) {
+			cBC.createCorso(corso);
+			System.out.println(corso.toString());
+			
+			corBC.createOrUpdate(corsista);
+			System.out.println(corsista.toString());
+			
+			cc.setCodCorso(corso.getCodCorso());
+			cc.setCodCorsista(corsista.getCodiceCorsista());
+			ccBC.create(cc);
+			System.out.println(cc.toString());
+			
+		}catch (ClassNotFoundException | DAOException | IOException e) {
 			e.printStackTrace();
 			fail("Motivo: " + e.getMessage());
 		}
-		
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		try {
+			corBC.delete(corsista);
+			cBC.deleteCorso(corso);
+			
+			//Delete Docente
 			Statement stmt = conn.createStatement();
-			stmt.executeUpdate("Delete from docente where cod_docente=3");
-			stmt.executeUpdate("Delete from docente where cod_docente=4");
+			stmt.executeUpdate("Delete from docente where cod_docente=1");
 			conn.commit();
-			CorsistiDAO.getFactory().delete(conn, corsista);
-			CorsoDAO.getFactory().delete(conn, corso);
-			DBAccess.closeConnetion();
 		}catch (DAOException e) {
 			e.printStackTrace();
 			fail("Motivo: " + e.getMessage());
 		}
+		
 	}
 
+	
 
 }

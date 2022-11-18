24613c3f38ed7392432803844ec011860b990df2
Betacom <Betacom@Betacom-PCDMTT.homenet.telecomitalia.it>
2022-11-18 16:04:40 +0100

    Footer aggiornato

--- GestioneCorsi/SQL/InsertSingoli/insert_corso.sql
+++ GestioneCorsi/SQL/InsertSingoli/insert_corso.sql
@@ -1,9 +1,9 @@
 --prima esegui insert_docente
 
-insert into corso values(seq_corso.nextval, 'Java', '15-FEB-2022', '20-MAR-2022', 150, 'Impara Java', 'N5', 1, 4);
-insert into corso values(seq_corso.nextval, 'Html', '17-MAR-2023', '20-APR-2023', 150, 'Impara Html', 'N3', 1, 7);
-insert into corso values(seq_corso.nextval, 'Php', '17-MAR-2023', '20-APR-2023', 150, 'Impara Html', 'N3', 1, 7);
-insert into corso values(seq_corso.nextval, 'Css', '17-MAR-2023', '20-APR-2023', 150, 'Impara Html', 'N3', 1, 7);
-insert into corso values(seq_corso.nextval, 'Prova4', '17-MAR-2023', '20-APR-2023', 150, 'Impara Html', 'N3', 1, 7);
-insert into corso values(seq_corso.nextval, 'Prova5', '17-MAR-2023', '20-APR-2023', 150, 'Impara Html', 'N3', 1, 7);
+insert into corso values(seq_corso.nextval, 'Java', '15-FEB-2022', '20-MAR-2022', 150, 'Impara Java', 'N5', 1, 0);
+insert into corso values(seq_corso.nextval, 'Html', '17-MAR-2023', '20-APR-2023', 250, 'Impara Html', 'N3', 2, 0);
+insert into corso values(seq_corso.nextval, 'Php', '17-MAR-2023', '20-APR-2023', 50, 'Impara Php', 'N3', 2, 0);
+insert into corso values(seq_corso.nextval, 'Css', '17-MAR-2023', '20-APR-2023', 350, 'Impara Css', 'N3', 3, 0);
+insert into corso values(seq_corso.nextval, 'Inglse', '17-MAR-2023', '20-APR-2023', 100, 'Inglese B1', 'N3', 4, 0);
+insert into corso values(seq_corso.nextval, 'Python', '17-MAR-2023', '20-APR-2023', 140, 'Impara Python', 'N3', 1, 0);
 commit
--- GestioneCorsi/SQL/InsertSingoli/insert_docente.sql
+++ GestioneCorsi/SQL/InsertSingoli/insert_docente.sql
@@ -1,4 +1,6 @@
 insert into docente values('Marco', 'Rossi', 'ass', 1);
-insert into docente values('Maria', 'Bianchi', 'cv', 2);
+insert into docente values('Bianca', 'Bianchi', 'cv', 2);
+insert into docente values('Elisa', 'Neri', 'ass', 3);
+insert into docente values('Francesco', 'Verdi', 'cv', 4);
 
 commit
